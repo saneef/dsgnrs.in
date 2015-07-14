bfa9ecb1e4fe022ed8cdb72db51ef908ee608946
Saneef Ansari <saneef@gmail.com>
2015-07-14 21:15:49 +0530

    Curation pages only available to admins

--- app/controllers/curate_controller.rb
+++ app/controllers/curate_controller.rb
@@ -1,16 +1,20 @@
 class CurateController < ApplicationController
   #GET /:status
   def index
-    logger.debug "Listing users from: #{params[:status]}" if params[:status]
-    case params[:status]
-    when 'waiting'
-      @users = User.all_waiting
-    when 'rejected'
-      @users = User.all_rejected
-    when 'approved'
-      redirect_to root_url
+    if current_user && current_user.is_admin?
+      logger.debug "Listing users from: #{params[:status]}" if params[:status]
+      case params[:status]
+      when 'waiting'
+        @users = User.all_waiting
+      when 'rejected'
+        @users = User.all_rejected
+      when 'approved'
+        redirect_to root_url
+      else
+        not_found
+      end
     else
-      not_found
+      permission_denied
     end
   end
 end
